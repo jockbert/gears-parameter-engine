f497ba04c89b81dc97e394e81b96b23cf74ca84f
Joakim Akerblom <per.joakim.eriksson@gmail.com>
2018-11-24 13:48:31 +0100

    Extract BisectingRelation to separate file

---
+++ src/main/scala/se/mjukomp/gears/BisectingRelation.scala
@@ -0,0 +1,64 @@
+package se.mjukomp.gears
+
+import Bisection.backtrack
+import Bisection.backtrackMaxValue
+import Bisection.backtrackMinValue
+
+case class OperationalDomain(source: Range, target: Range) {
+
+  /**
+   * Calculates the valid function domain (source range),
+   * given the unrelated source and target ranges, translation
+   * function and backtrack preference.
+   */
+  def mergeDomains(
+    fn:         MonotoneFn,
+    preference: BacktrackPreference): Either[RelationError, OperationalDomain] = {
+    val narrowedTarget = source
+      .map(fn).intersection(target)
+
+    narrowedTarget match {
+      case None => Left(NoRangeOverlap)
+      case Some(newTarget) =>
+        val newSource = Range(
+          backtrack(newTarget.min, fn, source, preference),
+          backtrack(newTarget.max, fn, source, preference))
+
+        Right(OperationalDomain(newSource, newTarget))
+    }
+  }
+}
+
+/** A relation that backtrack source value via bisection. */
+case class BisectingRelation(
+  source:     Amount,
+  fn:         MonotoneFn,
+  domain:     OperationalDomain,
+  target:     Amount,
+  preference: BacktrackPreference)
+  extends Relation {
+
+  var blockFeedback: Boolean = false
+
+  def withoutFeedback(changeFn: () => Unit): Unit =
+    if (blockFeedback) ()
+    else {
+      blockFeedback = true
+      changeFn()
+      blockFeedback = false
+    }
+
+  val sourceValueListener: ValueListener =
+    (value) => withoutFeedback(() => target.set(fn(value)))
+
+  val targetValueListener: ValueListener =
+    (value) => withoutFeedback(() => {
+      val preferedValue = preference.apply(
+        () => backtrackMinValue(target.get(), fn, domain.source),
+        () => backtrackMaxValue(target.get(), fn, domain.source))
+      source.set(preferedValue)
+    })
+
+  source.addListener(sourceValueListener)
+  target.addListener(targetValueListener)
+}
--- src/main/scala/se/mjukomp/gears/Relation.scala
+++ src/main/scala/se/mjukomp/gears/Relation.scala
@@ -8,25 +8,6 @@ import BacktrackPreference._
 sealed trait RelationError
 case object NoRangeOverlap extends RelationError
 
-case class OperationalDomain(source: Range, target: Range) {
-  def mergeDomains(
-    fn:         MonotoneFn,
-    preference: BacktrackPreference) = {
-    val narrowedTarget = source
-      .map(fn).intersection(target)
-
-    narrowedTarget match {
-      case None => Left(NoRangeOverlap)
-      case Some(newTarget) =>
-        val newSource = Range(
-          backtrack(newTarget.min, fn, source, preference),
-          backtrack(newTarget.max, fn, source, preference))
-
-        Right(OperationalDomain(newSource, newTarget))
-    }
-  }
-}
-
 case class RelationBuilder(target: Parameter) {
 
   def asFunctionOf(
@@ -78,40 +59,7 @@ object Relation {
 }
 
 /** The relation between two values, given a relation function. */
-sealed trait Relation {
+trait Relation {
   def source: Amount
   def target: Amount
-}
-
-case class BisectingRelation(
-  source:     Amount,
-  fn:         MonotoneFn,
-  domain:     OperationalDomain,
-  target:     Amount,
-  preference: BacktrackPreference)
-  extends Relation {
-
-  var blockFeedback: Boolean = false
-
-  def withoutFeedback(changeFn: () => Unit): Unit =
-    if (blockFeedback) ()
-    else {
-      blockFeedback = true
-      changeFn()
-      blockFeedback = false
-    }
-
-  val sourceValueListener: ValueListener =
-    (value) => withoutFeedback(() => target.set(fn(value)))
-
-  val targetValueListener: ValueListener =
-    (value) => withoutFeedback(() => {
-      val preferedValue = preference.apply(
-        () => backtrackMinValue(target.get(), fn, domain.source),
-        () => backtrackMaxValue(target.get(), fn, domain.source))
-      source.set(preferedValue)
-    })
-
-  source.addListener(sourceValueListener)
-  target.addListener(targetValueListener)
 }
